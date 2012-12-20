d2cecbfcb2014d8eaebce9c401f10e92a608ae8e
Antony <cat007@land.ru>
2012-12-20 17:57:30 +0600

    Update translate/toptank_en.php
    исправил опечатку

--- translate/toptank_en.php
+++ translate/toptank_en.php
@@ -1,5 +1,5 @@
 <?php
-  $lang['toptank_1'] = 'Totsl<br />tanks';
+  $lang['toptank_1'] = 'Total<br />tanks';
   $lang['toptank_2'] = 'No tanks selected to show. Go to <a href="./admin/" target="_blank">admin area</a> and add tanks to show.';
   $lang['toptank_3'] = 'Amount<br />in clan';
 ?>
