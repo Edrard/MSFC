2597a323d34723f87ac8ca613ef09eb95495146a
Antony <287135814@qip.ru>
2013-01-14 00:56:01 +0600

    обновил имена для табов
    процентов 90% охватил которые на данный момент используются

--- admin/translate/tabs.php
+++ admin/translate/tabs.php
@@ -11,56 +11,58 @@
     * @copyright   2011-2012 Edd - Aleksandr Ustinov
     * @link        http://wot-news.com
     * @package     Clan Stat
-    * @version     $Rev: 2.0.0 $
+    * @version     $Rev: 2.1.6 $
     *
     */
 ?>
 <?php
-    $tabs_lang['ru']['roster'] =  'Состав'; 
     $tabs_lang['ru']['active'] =  'Активность общая';
     $tabs_lang['ru']['active_1'] =  'Активность награды';
-    $tabs_lang['ru']['best'] =  'Лучшие результаты';
-    $tabs_lang['ru']['overall'] =  'Общие результаты';
-    $tabs_lang['ru']['perform_all'] =  'Боевая эффективность'; 
-    $tabs_lang['ru']['average_perform'] =  'Средняя производительность'; 
-    $tabs_lang['ru']['battel'] =  'Боевой опыт'; 
-
-    $tabs_lang['ru']['rating_all'] =  'Рейтинг';
-    $tabs_lang['ru']['ajaxtank'] =  'Техника';
-
-
-    $tabs_lang['ru']['medals_all'] =  'Награды';
-
+    $tabs_lang['ru']['ajax_activity'] =  'Активность игроков';
+    $tabs_lang['ru']['ajax_general_activity'] = 'Общая за период';
+    $tabs_lang['ru']['ajax_player'] = 'Динамика игрока';
+    $tabs_lang['ru']['ajaxstat'] = 'Танковые показатели';
+    $tabs_lang['ru']['ajaxtank'] = 'Техника';
     $tabs_lang['ru']['aval_top_tank'] =  'Наличие танков';
-
-
+    $tabs_lang['ru']['avt'] = 'Приветственное';
+    $tabs_lang['ru']['battel'] =  'Боевой опыт'; 
+    $tabs_lang['ru']['best'] =  'Лучшие результаты';
     $tabs_lang['ru']['gk'] =  'Блокированная техника';
+    $tabs_lang['ru']['medals_all'] =  'Награды';
+    $tabs_lang['ru']['overall'] =  'Общие результаты';
+    $tabs_lang['ru']['perform_all'] =  'Боевая эффективность';  
+    $tabs_lang['ru']['player'] =  'Не использовать';
+    $tabs_lang['ru']['rating_all'] =  'Рейтинг';
+    $tabs_lang['ru']['roster'] =  'Состав';
+    $tabs_lang['ru']['topteh'] =  'Топ-техника';
 
     $tabs_lang['ru']['poss'] =  'Собственность клана'; 
     $tabs_lang['ru']['attack'] =  'Запланированные атаки'; 
 
-
     //Eng
-    $tabs_lang['en']['roster'] =  'Staff'; 
     $tabs_lang['en']['active'] =  'Overall activity';
     $tabs_lang['en']['active_1'] =  'Medals activity';
-    $tabs_lang['en']['best'] =  'Best results';
-    $tabs_lang['en']['overall'] =  'Overall Results';
-    $tabs_lang['en']['perform_all'] =  'Battle Performance'; 
-    $tabs_lang['en']['average_perform'] =  'Average performance'; 
-    $tabs_lang['en']['battel'] =  'Battle Experience'; 
-
-    $tabs_lang['en']['rating_all'] =  'Rating';
+    $tabs_lang['en']['ajax_activity'] =  'Players activity';
+    $tabs_lang['en']['ajax_general_activity'] = 'Overall for period';
+    $tabs_lang['en']['ajax_player'] = 'Players detail';
+    $tabs_lang['en']['ajaxstat'] = 'Tanks detail';
     $tabs_lang['en']['ajaxtank'] =  'Vehicles';
 
-
-    $tabs_lang['en']['medals_all'] =  'Achievements';
-
     $tabs_lang['en']['aval_top_tank'] =  'Presented tanks';
-
-
+    $tabs_lang['en']['avt'] = 'Greeting';
+    $tabs_lang['en']['battel'] =  'Battle Experience';  
+    $tabs_lang['en']['best'] =  'Best results';
     $tabs_lang['en']['gk'] =  'Blocked Vehicles';
+    $tabs_lang['en']['medals_all'] =  'Achievements';
+    $tabs_lang['en']['overall'] =  'Overall Results';
+    $tabs_lang['en']['perform_all'] =  'Battle Performance';
+    $tabs_lang['en']['player'] =  'Dont use';
+    $tabs_lang['en']['rating_all'] =  'Rating';
+    $tabs_lang['en']['roster'] =  'Staff'; 
+    $tabs_lang['en']['topteh'] =  'Top Vehicles';
+
 
     $tabs_lang['en']['poss'] =  'Clan possession'; 
-    $tabs_lang['en']['attack'] =  'Planning attacks'; 
+    $tabs_lang['en']['attack'] =  'Planning attacks';  
+
 ?>
