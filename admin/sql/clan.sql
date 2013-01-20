43b2b250aeae337fbc93f382e84014c9853345c9
Antony <cat007@land.ru>
2013-01-20 16:38:53 +0600

    Update admin/sql/clan.sql
    а вот тут то я и забыл поправить для корректного переключения интерфейса

--- admin/sql/clan.sql
+++ admin/sql/clan.sql
@@ -188,7 +188,7 @@ INSERT INTO `config` (`name`, `value`) VALUES
 ('server', 'ru'),
 ('clan', '37'),
 ('cache', '12'),
-('align', 'ver'),
+('themeинтерфейс', 'ui-lightness'),
 ('pars', 'curl'),
 ('time', '0'),
 ('cron', '1'),
