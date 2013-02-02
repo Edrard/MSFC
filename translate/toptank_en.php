510934ef95c2ea8b87c511216b776f78bd5a385f
Exinaus <exinaus@gmail.com>
2013-02-02 18:31:00 +0200

    Правка стилей
    Поправлены стили для кнопок, состоящих из одних иконок, и стиль для
    стрелок сортировки в таблицах.

--- translate/toptank_en.php
+++ translate/toptank_en.php
@@ -16,7 +16,7 @@
     */
 ?>
 <?php
-  $lang['toptank_1'] = 'Total<br />tanks';
+  $lang['toptank_1'] = 'In total';
   $lang['toptank_2'] = 'No tanks selected to show. Go to <a href="./admin/" target="_blank">admin area</a> and add tanks to show.';
   $lang['toptank_3'] = 'Amount<br />in clan';
   $lang['toptank_4'] = 'Index: ';
--- translate/toptank_ru.php
+++ translate/toptank_ru.php
@@ -16,8 +16,8 @@
     */
 ?>
 <?php
-  $lang['toptank_1'] = 'Кол-во<br />танков';
+  $lang['toptank_1'] = 'Всего';
   $lang['toptank_2'] = 'Не выбрано ни одного танка. Зайдите в <a href="./admin/" target="_blank">админку</a> и добавьте танков для отображения.';
   $lang['toptank_3'] = 'Всего';
-  $lang['toptank_4'] = 'Индекс: ';  
+  $lang['toptank_4'] = 'Индекс: ';
 ?>
