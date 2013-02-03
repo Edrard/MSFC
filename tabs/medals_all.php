2be61a18fee174d49d4694bbfeacff8ebe431060
Antony <287135814@qip.ru>
2013-02-03 13:51:35 +0600

    Косметика 2
    Забыл добавить пару файлов

--- tabs/medals_all.php
+++ tabs/medals_all.php
@@ -96,7 +96,7 @@
                 <?php foreach($res[$rand_keys]['medals'] as $tmed => $hkey){
                         foreach($hkey as $tm => $column) { ?>
                         <th align='center' valign='top' class="{sorter: 'digit'} bb allmedalhide <?=$tmed;?>show"
-                        <?php echo 'title="<table width=\'100%\' border=\'0\' cellspacing=\'0\' cellpadding=\'0\' class=\'ui-widget ui-widget-content\' style=\'border: 0px !important;\'><tr><td><img src=\'./'.$column['img'].'\' /></td><td><span align=\'center\' style=\'font-weight: bold;\'>'.$column['title'].'.</span><br> '.$lang['title_'.$tm].'</td></tr></table>"';?>
+                        <?php echo 'title="<table cellspacing=\'0\' cellpadding=\'0\' style=\'border: 0px !important;\'><tr><td><img src=\'./'.$column['img'].'\' /></td><td><span align=\'center\' style=\'font-weight: bold;\'>'.$column['title'].'.</span><br> '.$lang['title_'.$tm].'</td></tr></table>"';?>
                         >
                             <?php echo '<img src="'.$column['img'].'" style="width:60px;" /><br>'.$column['title']; ?>
                         </th>
