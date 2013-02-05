bcb7df90dba2c3f786b498588ed3b2ea060e9989
Antony <287135814@qip.ru>
2013-02-06 05:03:15 +0600

    откат на дефолтный mysql
    Случайно скопировал

--- function/mysql.php
+++ function/mysql.php
@@ -22,12 +22,12 @@
         exit;
     }
 
-    $dbhost ='mysql.hostinger.ru';
+    $dbhost ='localhost';
     // username and password to log onto db SERVER
-    $dbuser ='u343566427_shw';
-    $dbpass  ='cat$5181';
+    $dbuser ='root';
+    $dbpass  ='';
     // name of database
-    $dbname='u343566427_stat';
+    $dbname='';
     //en - Prefix must be min 1 symbol, max 5 symbols, with _ at the end. Only a-z, A-Z and numbers allowed. For example: $dbprefix = 'msfc_';
     //ru - Префикс должен быть не менее 1 и не более 5 символов, в конце префикса должен быть символ _. Разрешены только английские буквы и цифры.
     //Для примера: $dbprefix = 'msfc_';
