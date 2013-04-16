9d48c3d8b88adf34cccdebe9be1bf2081249749d
Exinaus <exinaus@gmail.com>
2013-04-16 13:53:10 +0300

    Мелочи.
    Переход на сайт wot-news.com из копирайта в новом окне/вкладке. По вот
    этой просьбе:
    http://wot-news.com/forum/viewtopic.php?f=30&t=14667&view=unread#p28514

--- views/body.php
+++ views/body.php
@@ -157,7 +157,7 @@
             <tr>
                 <td valign="top" colspan="6">
                     <div align="center" class="ui-widget ui-widget-content ui-corner-bottom">
-                        © 2011-<?=date('Y') ?> <a href="http://wot-news.com/">Wot-news.com</a> <?=$lang['version']; ?> <?php echo VER; ?><br>
+                        © 2011-<?=date('Y') ?> <a href="http://wot-news.com/" target="_blank">Wot-news.com</a> <?=$lang['version']; ?> <?php echo VER; ?><br>
                         <?php $end_time = microtime(true); echo $lang['ex_time'],' - ',round($end_time - $begin_time,4),' ',$lang['sec']; ?><br>
                         <?php if(isset($memory_usage)){echo $lang['memory'],' ',$memory_usage;} ?>
                     </div>
