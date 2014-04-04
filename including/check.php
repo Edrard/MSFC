8155568680d83ab1bbe035d59a6bf5f5d0cf7cc0
Exinaus <exinaus@gmail.com>
2014-04-04 13:22:21 +0300

    Сообщение о ошибке
    Добавил отображение сообщения о ошибке если на хостинге отключен cURL.

--- including/check.php
+++ including/check.php
@@ -163,12 +163,16 @@ $lang['b_chmod_off'] = 'Directory <b>/cache/players</b> doesn\'t exist, or no pe
 $lang['c_chmod_off'] = 'Directory <b>/admin/sql</b> doesn\'t exist, or no permission to write. <br /> Директория <b>/admin/sql</b> не существует, или невозможна запись.';
 $lang['d_chmod_off'] = 'Directory <b>/cache/other/</b> doesn\'t exist, or no permission to write. <br /> Директория <b>/cache/other/</b> не существует, или невозможна запись.';
 $lang['e_chmod_off'] = 'No permission to create files in root directory, pls change permission to 777 to setup mysql connection. <br> Нет доступа на создание файлов в корневой директории модуля, пожалуйста измените права доступа на 777 для настройки MySQL соединения';
+$lang['curl_off'] = '<i>ERROR:</i> <strong>cURL</strong> extention do not loaded, without it this site can\'t function.<br /> В настройках PHP вашего хостинга отключено использование расширения <strong>cURL</strong>, без него Модуль Статистики не будет работать.';
 
 if ( !extension_loaded('pdo') ) {
     show_message($lang['pdo_off']);
 }
 if ( !extension_loaded('pdo_mysql') ) {
     show_message($lang['pdo_mysql_off']);
+}
+if (!extension_loaded("curl")) {
+   show_message($lang['curl_off']);
 }
 if(ini_get('short_open_tag') != 1) {
     show_message($lang['short_tag_off']);
