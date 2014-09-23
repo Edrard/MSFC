ae96f1688d76207a32fb327b21929a210ff30257
Exinaus <exinaus@gmail.com>
2014-09-23 19:42:09 +0300

    upd

--- admin/sql/clan.sql
+++ admin/sql/clan.sql
@@ -159,7 +159,7 @@ INSERT INTO `config` (`name`, `value`) VALUES
 ('new_players', '172800'),
 ('main_progress', '172800'),
 ('medal_progress', '172800'),
-('version', '310.7'),
+('version', '311.0'),
 ('new_tanks', '172800'),
 ('application_id', 'demo'),
 ('company', '0'),
