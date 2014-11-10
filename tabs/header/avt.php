f4fb4d8650c799f5dd0f1faa4dc5f4bd7faea648
Antony <287135814@qip.ru>
2014-11-11 01:24:49 +0500

    Мелочевка
    исправление в вызове метода сортировки

--- tabs/header/avt.php
+++ tabs/header/avt.php
@@ -1,8 +1,4 @@
 
 $("#avt1").tablesorter({headers:{ 0: { sorter: false}, 1: {sorter: false}, 2: {sorter: false}}, widgetOptions: {uitheme : 'bootstrap'}});
-<? for ($i=2; $i<=6; $i++) { ?>
-$("#avt<?=$i;?>").tablesorter({headers:{ 0: { sorter: false}, 1: {sorter: false} }, sortList:[[1,0]], widgetOptions: {uitheme : 'bootstrap'}});
-<? }
-   for ($i=7; $i<=9; $i++) { ?>
-$("#avt<?=$i;?>").tablesorter({headers:{ 0: { sorter: false}, 1: {sorter: false} }, sortList:[[0,0]], widgetOptions: {uitheme : 'bootstrap'}});
-<? } ?>
+$("#avt2").add("#avt3").add("#avt4").add("#avt5").add("#avt6").add("#avt7").add("#avt8").add("#avt9").tablesorter({headers:{ 0: { sorter: false}, 1: {sorter: false} }, sortList:[[1,0]], widgetOptions: {uitheme : 'bootstrap'}});
+$("#avt7").add("#avt8").add("#avt9").trigger("sorton",[[[0,0]]]);
