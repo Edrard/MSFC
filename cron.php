9a525eb56a16be30e34cff68e7f92de94cc74362
Exinaus <exinaus@gmail.com>
2015-01-14 02:42:44 +0200

    fix
    Исправле запрос в БД.

--- cron.php
+++ cron.php
@@ -152,7 +152,7 @@ if (($multi_prefix[$dbprefix]['cron'] + $config['cron_time']*3600) <= now() ){
                     $counter = $error_messages = array();
                     $counter['old'] = array();
                     //get list of members in DB, if some of theme already there from previous unsucessfull runs
-                    $tmp = $db->select('SELECT account_id FROM `col_players` WHERE updated_at >= "'.(now()-$config['cron_time']*60*60).'" ORDER BY updated_at DESC;',__line__,__file__,'column');
+                    $tmp = $db->select('SELECT account_id FROM `col_players` WHERE updated_at >= "'.(now()-$config['cron_time']*60*60).'" ORDER BY updated_at DESC;',__line__,__file__,'rows');
                     if(!empty($tmp)) {
                      $counter['old'] = array_flip($tmp);
                     }
